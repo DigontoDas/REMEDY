c0aca8732cb89fc8e81a3b51a04e4d33bd016f4f
Digonto Das <118357100+sofaking420@users.noreply.github.com>
2024-08-02 01:41:42 +0600

    Delete Admin_login.js

--- Admin_login.js
+++
@@ -1,7 +0,0 @@
-document.querySelector('form').addEventListener('submit', function(e) {
-    e.preventDefault();
-    const username = document.getElementById('username').value;
-    const email = document.getElementById('email').value;
-    const password = document.getElementById('password').value;
-    console.log(`Username: ${username}, Email: ${email}, Password: ${password}`);
-});
