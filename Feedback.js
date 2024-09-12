c25898d65cb7613a9fd79fd2edfa7028e11e004b
Digonto Das <118357100+sofaking420@users.noreply.github.com>
2024-09-12 22:14:20 +0600

    Delete Feedback.js

--- Feedback.js
+++
@@ -1,7 +0,0 @@
-document.querySelector('form').addEventListener('submit', function(e) {
-    e.preventDefault();
-    const role = document.querySelector('input[name="role"]:checked').value;
-    const rating = document.getElementById('rating').value;
-    const comment = document.getElementById('comment').value;
-    console.log(`Role: ${role}, Rating: ${rating}, Comment: ${comment}`);
-});
