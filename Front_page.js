4189b7e7b4cbbb76ff3cfb5b4df4051f138aa693
Digonto Das <118357100+sofaking420@users.noreply.github.com>
2024-09-12 21:50:31 +0600

    Delete Front_page.js

--- Front_page.js
+++
@@ -1,6 +0,0 @@
-const toggleButtons = document.querySelectorAll('input[name="role"]');
-toggleButtons.forEach(button => {
-    button.addEventListener('change', function() {
-        document.body.style.backgroundColor = this.value === 'patient' ? 'red' : 'blue';
-    });
-});
